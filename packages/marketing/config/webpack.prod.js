b838f323ea42ae22cd25bdac0a6b71e4bce566f1
toahabhuiyan01 <toahabhuiyan@gmail.com>
2021-04-13 11:10:35 +0600

    Created container deploy script

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,11 +6,13 @@ const commonConfig = require('./webpack.common');
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'marketing',
+            filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp': './src/bootstrap'
             },
