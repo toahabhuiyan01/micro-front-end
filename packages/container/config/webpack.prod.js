6b262ce0d99ba9fae02ab751d0795df051101347
toahabhuiyan01 <toahabhuiyan@gmail.com>
2021-04-15 12:57:43 +0600

    add amplify conf

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,8 +15,8 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}/auth/latest/remoteEntry.js`
+                marketing: `marketing@/marketing/latest/remoteEntry.js`,
+                auth: `auth@/auth/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
